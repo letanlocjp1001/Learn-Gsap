2883d752a6d354af19ed9c79ea367e14e1ba22f3
letanloc <letanlocjp1001@gmail.com>
2023-06-16 01:31:37 +0900

    learn gsap

---
+++ 01/js/app.js
@@ -0,0 +1,64 @@
+const tl = gsap.timeline()
+tl.from('.left-side-container img', {
+  y: '-100%',
+  duration: 2.5,
+  transform: 'scale(1, 0.9)',
+  delay: 1,
+})
+tl.from(
+  '.right-side-container img',
+  {
+    y: 400,
+    duration: 2.5,
+    transform: 'scale(1.5, 1.3)',
+    stagger: {
+      amount: 1.4,
+    },
+  },
+  '-=2.5'
+)
+tl.from(
+  '.right-side-container h6',
+  {
+    y: 500,
+    duration: 2,
+  },
+  '-=2.5'
+)
+
+let innerTextContent = document.getElementById('innerTextContent')
+
+const imgs = document.querySelectorAll('.imgHover')
+
+imgs.forEach((img) => {
+  img.addEventListener('mouseover', function contentChange(el) {
+    // console.log(el)
+    if (el.target.id === 'img1') {
+      finalAnimation(el.target.id, 'Le Oh Diva Impulse No1')
+    } else if (el.target.id === 'img2') {
+      finalAnimation(el.target.id, 'Le Oh Flase')
+    } else if (el.target.id === 'img3') {
+      finalAnimation(el.target.id, 'Le Oh Apache')
+    } else if (el.target.id === 'img4') {
+      finalAnimation(el.target.id, ' Ruudiere')
+    } else if (el.target.id === 'img5') {
+      finalAnimation(el.target.id, 'Le Oh Diva Rossi')
+    }
+  })
+  function finalAnimation(id, text) {
+    let imgspan = document.getElementById('left-side-img-span')
+    innerTextContent.innerText = text
+    let img = document.createElement('img')
+    img.src = `./images/${id}.jpg`
+    imgspan.appendChild(img)
+
+    var imgTags = imgspan.querySelectorAll('img')
+    var lastImgTag = imgTags[imgTags.length - 1]
+    gsap.from(lastImgTag, {
+      y: -800,
+      duration: 2,
+      transform: 'scale(1, 0.8)',
+      ease: 'Power3.easeOut',
+    })
+  }
+})
