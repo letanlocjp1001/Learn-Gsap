baf6b2688662d77d56bae342617d3219bcdf9681
letanloc <letanlocjp1001@gmail.com>
2023-07-23 02:07:37 +0900

    08 Portfolio

---
+++ 08/js/app.js
@@ -0,0 +1,31 @@
+// Logo animation
+let textLogo = document.querySelector('.logo')
+
+const tl = gsap.timeline({
+  defaults: {
+    ease: 'power4.inOut',
+  },
+})
+
+textLogo.innerHTML = textLogo.textContent.replace(
+  /\S/g,
+  "<span class='letter'>$&</span>"
+)
+
+tl.from('.letter', 0.7, {
+  y: -50,
+  x: 50,
+  rotation: 20,
+  autoAlpha: 0,
+  stagger: 0.1,
+}).from('.nav_list', 0.7, {
+  y: 50,
+  x: -50,
+  rotation: 180,
+  autoAlpha: 0,
+  stagger: 0.1,
+})
+
+particlesJS.load('particles-js', 'particles.json', function () {
+  console.log('callback - particles.js config loaded')
+})
