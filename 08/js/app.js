a0a1b566b7a64c0d223c97ac8945651ab1fb22aa
letanloc <letanlocjp1001@gmail.com>
2023-07-25 01:43:16 +0900

    修正

--- 08/js/app.js
+++ 08/js/app.js
@@ -18,13 +18,27 @@ tl.from('.letter', 0.7, {
   rotation: 20,
   autoAlpha: 0,
   stagger: 0.1,
-}).from('.nav_list', 0.7, {
-  y: 50,
-  x: -50,
-  rotation: 180,
-  autoAlpha: 0,
-  stagger: 0.1,
-})
+}).from(
+  '.nav_list',
+  0.7,
+  {
+    y: 50,
+    x: -50,
+    rotation: 180,
+    autoAlpha: 0,
+    stagger: 0.1,
+  },
+  '-=1'
+)
+tl.from(
+  '.home_left',
+  1.5,
+  {
+    x: '-100%',
+    autoAlpha: 0,
+  },
+  '-=1'
+)
 
 particlesJS.load('particles-js', 'particles.json', function () {
   console.log('callback - particles.js config loaded')
