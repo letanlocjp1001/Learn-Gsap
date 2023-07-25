8cb78af7ab36280feca867044bd70cd12292f2af
letanloc <letanlocjp1001@gmail.com>
2023-07-26 08:34:26 +0900

    update

--- 08/js/app.js
+++ 08/js/app.js
@@ -1,44 +1,44 @@
 // Logo animation
-let textLogo = document.querySelector('.logo')
+// let textLogo = document.querySelector('.logo')
 
-const tl = gsap.timeline({
-  defaults: {
-    ease: 'power4.inOut',
-  },
-})
+// const tl = gsap.timeline({
+//   defaults: {
+//     ease: 'power4.inOut',
+//   },
+// })
 
-textLogo.innerHTML = textLogo.textContent.replace(
-  /\S/g,
-  "<span class='letter'>$&</span>"
-)
+// textLogo.innerHTML = textLogo.textContent.replace(
+//   /\S/g,
+//   "<span class='letter'>$&</span>"
+// )
 
-tl.from('.letter', 0.7, {
-  y: -50,
-  x: 50,
-  rotation: 20,
-  autoAlpha: 0,
-  stagger: 0.1,
-}).from(
-  '.nav_list',
-  0.7,
-  {
-    y: 50,
-    x: -50,
-    rotation: 180,
-    autoAlpha: 0,
-    stagger: 0.1,
-  },
-  '-=1'
-)
-tl.from(
-  '.home_left',
-  1.5,
-  {
-    x: '-100%',
-    autoAlpha: 0,
-  },
-  '-=1'
-)
+// tl.from('.letter', 0.7, {
+//   y: -50,
+//   x: 50,
+//   rotation: 20,
+//   autoAlpha: 0,
+//   stagger: 0.1,
+// }).from(
+//   '.nav_list',
+//   0.7,
+//   {
+//     y: 50,
+//     x: -50,
+//     rotation: 180,
+//     autoAlpha: 0,
+//     stagger: 0.1,
+//   },
+//   '-=1'
+// )
+// tl.from(
+//   '.home_left',
+//   1.5,
+//   {
+//     x: '-100%',
+//     autoAlpha: 0,
+//   },
+//   '-=1'
+// )
 
 particlesJS.load('particles-js', 'particles.json', function () {
   console.log('callback - particles.js config loaded')
